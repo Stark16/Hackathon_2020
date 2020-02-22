3743c9c5b23a4974b4b17cbf57fabfd64de7fb1c
Stark16 <pradyumnp508@gmail.com>
2020-02-22 19:43:02 +0530

    made it little bit more robust for detection using max area finding

--- Scripts and Data/finding_page.py
+++ Scripts and Data/finding_page.py
@@ -1,21 +1,29 @@
 import cv2
 import numpy as np
 
-im = cv2.imread("./Data/blank_4.jpg")
-im_name = cv2.resize(im, (600,700))
+
+im = cv2.imread("./Data/blank_simple (4).jpg")
+im_name = cv2.resize(im, (700, 700))
 gray = cv2.cvtColor(im_name, cv2.COLOR_BGR2GRAY)
+edge = cv2.Canny(gray, 100, 200, apertureSize=3)
 
-edge = cv2.Canny(gray, 100, 500, apertureSize=3)
-cv2.imshow("Adaptive", edge)
-cv2.waitKey(0)
 
-item,  hierchy = cv2.findContours(edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+item,  hierarchy = cv2.findContours(edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+
+area_cont = np.arange(len(item))
+i = 0
 for con in item:
-    area_cont = cv2.contourArea(con)
-    if area_cont > 1000:
-        cv2.drawContours(im_name, con, -1, (0,255,0), 3)
+    area_cont[i] = cv2.contourArea(con)
+    i += 1
 
 
+max_area = np.amax(area_cont)
+max_index = np.where(area_cont == max_area)
+print(max_area)
+
+
+cv2.drawContours(im_name, item, max_index[0] , (0,255,0), 3)
+cv2.imshow("Adaptive", edge)
 cv2.imshow("cont", im_name)
 cv2.waitKey(0)
