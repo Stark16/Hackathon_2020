e230bd88d57c97276fcdee64601081da2927cd64
Stark16 <pradyumnp508@gmail.com>
2020-03-02 16:55:32 +0530

    Added Convex hull script name page_detection_convexhull.py

--- Scripts and Data/page_detection_convexhull.py
+++ Scripts and Data/page_detection_convexhull.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-im_name = cv2.imread("./Data/blank_simple (3).jpg")
-im_name = cv2.resize(im_name, (700, 1200), interpolation=True)
+im_name = cv2.imread("./Data/blank_simple (2).jpg")
+im_name = cv2.resize(im_name, (700, 700), interpolation=True)
 gray = cv2.cvtColor(im_name, cv2.COLOR_BGR2GRAY)
 
 edge = cv2.Canny(gray, 100, 500, apertureSize=3)
@@ -19,9 +19,23 @@ for contour in item:
 max_area = np.amax(cont_area)
 max_index = np.where(cont_area == max_area)
 
-hull = cv2.convexHull(item[int(max_index[0])], clockwise=True)
-print(hull)
+page = np.empty_like(gray)
+page[:, :] = 0
 
-cv2.drawContours(im_name, hull, -1, (255, 255, 0), 3)
+cv2.drawContours(page, item, max_index[0], (255, 255, 255), 1)
+
+corners = cv2.goodFeaturesToTrack(page, 4, 0.01, 60)
+hull = cv2.convexHull(corners, clockwise=True)
+
+lines = []
+for i in range(4):
+    x1, y1 = hull[i].ravel()
+    if i == 3:
+        x2, y2 = hull[0].ravel()
+    else:
+        x2, y2 = hull[i+1].ravel()
+    lines.append([(x1, y1),  (x2, y2)])
+    cv2.line(im_name, (x1, y1), (x2, y2), (255, 255, 0), 3)
+
 cv2.imshow("pic", im_name)
 cv2.waitKey(0)
--- Scripts and Data/transforming_image_using_hough.py
+++ Scripts and Data/transforming_image_using_hough.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-
-im_name = cv2.imread("./Data/blank_simple (3).jpg")
+from matplotlib import pyplot
+im_name = cv2.imread("./Data/blank_simple (2).jpg")
 im_name = cv2.resize(im_name, (700, 700), interpolation=True)
 gray = cv2.cvtColor(im_name, cv2.COLOR_BGR2GRAY)
 
@@ -25,10 +25,16 @@ page[:, :] = 0
 cv2.drawContours(page, item, max_index[0], (255, 255, 255), 1)
 
 corners = cv2.goodFeaturesToTrack(page, 4, 0.01, 60)
-for corner in corners:
-    x, y = corner.ravel()
-    cv2.circle(im_name, (x, y), 5, (255, 200, 0), -1)
+hull = cv2.convexHull(corners, clockwise=True)
 
-cv2.imshow("pic", page)
-cv2.imshow("pic2", im_name)
+lines = []
+for i in range(4):
+    x1, y1 = hull[i].ravel()
+    if i == 3:
+        x2, y2 = hull[0].ravel()
+    else:
+        x2, y2 = hull[i+1].ravel()
+    lines.append([(x1, y1),  (x2, y2)])
+    cv2.line(im_name, (x1, y1), (x2, y2), (255, 255, 0), 3)
+
 cv2.waitKey(0)
